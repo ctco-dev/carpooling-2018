15dd4c59db2f95982a8e0cd9da8f30c9103c8f20
Margarita Sankova <margarita.sankova@ctco.lv>
2018-08-22 11:21:15 +0300

    add TripDto.java

---
+++ src/main/java/lv/ctco/javaschool/app/entity/domain/dto/TripDto.java
@@ -0,0 +1,15 @@
+package lv.ctco.javaschool.app.entity.domain.dto;
+
+import lv.ctco.javaschool.app.entity.domain.Place;
+import lv.ctco.javaschool.app.entity.domain.TripStatus;
+import lv.ctco.javaschool.auth.entity.domain.User;
+
+public class TripDto {
+    private User driver;
+    private Place from;
+    private Place to;
+    private int places;
+    private String time;
+    private boolean isEvent;
+    private TripStatus tripStatus;
+}

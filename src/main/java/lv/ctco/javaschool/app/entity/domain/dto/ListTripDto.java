503ccbca3b8eb7ad49e9684f8846c1f9fb9d0bd0
Yelena Silanteva <yelena.silanteva@ctco.lv>
2018-08-22 11:39:39 +0300

    - added getters and setters to ListTripDto

--- src/main/java/lv/ctco/javaschool/app/entity/domain/dto/ListTripDto.java
+++ src/main/java/lv/ctco/javaschool/app/entity/domain/dto/ListTripDto.java
@@ -1,8 +1,15 @@
 package lv.ctco.javaschool.app.entity.domain.dto;
 
-
 import java.util.List;
 
 public class ListTripDto {
     List<TripDto> trips;
+
+    public List<TripDto> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(List<TripDto> trips) {
+        this.trips = trips;
+    }
 }

943d3759a19d493c69cf146009a690c08afe7487
Aleksandr Porohin <alexander.porohin@ctco.lv>
2018-08-24 13:58:28 +0300

    added tests for AuthenticationApi class

--- src/test/java/lv/ctco/javaschool/auth/boundary/AuthenticationApiTest.java
+++ src/test/java/lv/ctco/javaschool/auth/boundary/AuthenticationApiTest.java
@@ -13,11 +13,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 
-import javax.security.enterprise.AuthenticationStatus;
 import javax.security.enterprise.SecurityContext;
 import javax.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
-import javax.security.enterprise.credential.Credential;
-import javax.security.enterprise.credential.UsernamePasswordCredential;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
@@ -42,29 +39,25 @@ class AuthenticationApiTest extends Mockito {
     }
 
     @Test
-    void login() {
-
-        AuthenticationApi aApi = new AuthenticationApi();
-
+    void login() throws Exception {
         User u = new User();
         u.setUsername("User1");
         u.setPassword("12345");
-
         UserLoginDto uld = new UserLoginDto();
         uld.setUsername(u.getUsername());
         uld.setPassword(u.getPassword());
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-
-        Credential credential = new UsernamePasswordCredential(uld.getUsername(), uld.getPassword());
-
-        when(securityContext.authenticate( request, response, AuthenticationParameters.withParams().credential(credential).newAuthentication(true)
-                .rememberMe(false))).thenReturn(AuthenticationStatus.SUCCESS);
-
-
-        assertEquals("User1", aApi.login(uld, request, response));
-
+        HttpServletRequest mockedRequest = mock(HttpServletRequest.class);
+        HttpServletResponse mockedResponse = mock(HttpServletResponse.class);
+        when(mockedRequest.getParameter("username")).thenReturn("testUsername");
+        when(mockedRequest.getParameter("password")).thenReturn("testPass");
+        Response response = Response
+                .status(Response.Status.UNAUTHORIZED)
+                .entity(new ErrorDto())
+                .build();
+        assertEquals(response.getStatus(), authApi.login(uld, mockedRequest, mockedResponse).getStatus());
+        verify(userStore, never()).createUser(anyString(), anyString(), anyString(), anyString(), anyString(), any(Role.class));
+        authApi.login(new UserLoginDto("testUserame", "testPass", "testName", "testSurname", "testPass"), mockedRequest, mockedResponse);
+        verify(securityContext, times(2)).authenticate(any(HttpServletRequest.class), any(HttpServletResponse.class), any(AuthenticationParameters.class));
     }
 
     @Test

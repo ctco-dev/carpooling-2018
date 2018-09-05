a2e32a6746333a14a0619fd5c958ac0fdaddfd93
oLyaa <olyash@inbox.lv>
2018-09-06 00:30:54 +0300

    in Main page -> changed function for table drawing

--- src/main/webapp/javascript/main.js
+++ src/main/webapp/javascript/main.js
@@ -7,11 +7,58 @@ function displayActiveTrips() {
         }
     }).then(function (response) {
         return response.json();
-    }).then(function (trips) {
-        console.log(JSON.stringify(trips));
-        w3DisplayData("trips", trips);
+    }).then(function (tripsList) {
+        console.log(JSON.stringify(tripsList));
+        drawTable(tripsList, "trips");
     });
 }
+
+function drawTable(tripsList, tabId){
+    var table = document.getElementById(tabId);
+    var tbody = table.getElementsByTagName('tbody')[0];
+    if (tbody) table.removeChild(tbody);
+
+    tbody = document.createElement('tbody');
+    tripsList.trips.forEach(function (e) {
+        var row = tbody.insertRow();
+        var cell1 = row.insertCell(0);
+        cell1.id="id";
+        cell1.classList.add("table_id");
+        cell1.innerHTML = e.id;
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = e.from+" - "+e.to;
+
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = e.driverInfo;
+
+        var cell4 = row.insertCell(3);
+        cell4.innerHTML = e.driverPhone;
+
+        var cell5 = row.insertCell(4);
+        cell5.id="places";
+        cell5.classList.add("table_places");
+        cell5.innerHTML =e.places.toString();
+
+        var cell6 = row.insertCell(5);
+        cell6.id="passengers";
+        cell6.classList.add("table_passengers");
+        cell6.innerHTML ="passengers";
+
+        var cell7 = row.insertCell(6);
+        cell7.innerHTML = e.event;
+
+        var cell8 = row.insertCell(7);
+        cell8.id="deleteButton";
+        cell8.innerHTML ="<button id=\"join-button\" type=\"button\" class=\"btn btn-primary\"\n" +
+            " onclick=\"join(this, $(this).closest('tr').find('.table_id').text(), $(this).closest('td').parent().index(), $(this).closest('tr').find('.table_places').text())\">\n" +
+            "Join</button>"
+    });
+    table.appendChild(tbody);
+}
+
+
+
 function scrollBar() {
     var table = document.getElementById("table-active-trip");
     var rows = document.getElementById("table-active-trip").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
